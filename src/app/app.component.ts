d10c30595b9fa83147001eb621a0b1ee7c3c872f
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-07-09 12:10:48 +0000

    Implements start game page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StartGameComponent } from './components/start-game/start-game.component';
+import { HowToPlayComponent } from './components/how-to-play/how-to-play.component';
 import { MainGameComponent } from './components/main-game/main-game.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StartGameComponent, MainGameComponent],
+  imports: [
+    RouterOutlet,
+    StartGameComponent,
+    HowToPlayComponent,
+    MainGameComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { StartGameComponent } from './components/start-game/start-game.component';
 import { MainGameComponent } from './components/main-game/main-game.component';
+import { HowToPlayComponent } from './components/how-to-play/how-to-play.component';
 
 export const routes: Routes = [
   { path: '', component: StartGameComponent },
+  { path: 'how-to-play', component: HowToPlayComponent },
   { path: 'main-game', component: MainGameComponent },
 ];
