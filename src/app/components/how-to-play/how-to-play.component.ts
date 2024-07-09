1f1ccdf3975d35bac2236ac87327b0057bcedffa
rougemeister <rougemeister1@gmail.com>
2024-07-09 06:30:29 +0000

    Added how to play page structure and styles

---
+++ src/app/components/how-to-play/how-to-play.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-how-to-play',
+  standalone: true,
+  imports: [],
+  templateUrl: './how-to-play.component.html',
+  styleUrl: './how-to-play.component.scss'
+})
+export class HowToPlayComponent {
+
+}
