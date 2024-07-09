61f282cd58f2ce5c892ee0a5239977d4054c7cf5
rougemeister <rougemeister1@gmail.com>
2024-07-09 11:28:49 +0000

    Added how to play component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StartGameComponent } from './components/start-game/start-game.component';
 import { MainGameComponent } from './components/main-game/main-game.component';
+import { HowToPlayComponent } from './components/how-to-play/how-to-play.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StartGameComponent, MainGameComponent],
+  imports: [RouterOutlet, StartGameComponent, MainGameComponent,HowToPlayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
