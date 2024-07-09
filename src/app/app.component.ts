3ecda039dc6edcddcce5c159c4a1edc2e863e0ce
rougemeister <rougemeister1@gmail.com>
2024-07-09 10:43:11 +0000

    Added button component and styled text to fit design file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HowToPlayComponent } from './components/how-to-play/how-to-play.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HowToPlayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/how-to-play/how-to-play.component.ts
+++ src/app/components/how-to-play/how-to-play.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BackButtonComponent } from '../back-button/back-button.component';
 
 @Component({
   selector: 'app-how-to-play',
   standalone: true,
-  imports: [],
+  imports: [BackButtonComponent],
   templateUrl: './how-to-play.component.html',
   styleUrl: './how-to-play.component.scss'
 })
