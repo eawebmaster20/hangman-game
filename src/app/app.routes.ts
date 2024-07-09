467645b80195d4f7d85dbe945e41e60788a39f31
Harriet Buadee <117269443+Harr-izle@users.noreply.github.com>
2024-07-09 10:54:06 -0700

    adds letters layout to app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,5 @@ import { AppComponent } from './app.component';
 import { StartGameComponent } from './components/start-game/start-game.component';
 
 export const routes: Routes = [
-    {path:'', component:StartGameComponent}
+    {path:'', component:StartGameComponent}  
 ];
