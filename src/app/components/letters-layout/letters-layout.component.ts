a0dce2d5ff4676b3a85b4b47442154eb2f63c00a
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-07-10 08:54:02 +0000

    Makes tweaks to letters layout

--- src/app/components/letters-layout/letters-layout.component.ts
+++ src/app/components/letters-layout/letters-layout.component.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-letters-layout',
   standalone: true,
   imports: [],
   templateUrl: './letters-layout.component.html',
-  styleUrl: './letters-layout.component.scss'
+  styleUrl: './letters-layout.component.scss',
 })
-export class LettersLayoutComponent {
-
-}
+export class LettersLayoutComponent {}
