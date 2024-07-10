4bda161145d07bc243521c58ee68a3516f8c8a12
Harriet Buadee <117269443+Harr-izle@users.noreply.github.com>
2024-07-09 17:02:31 -0700

    changes html template

--- src/app/components/letters-layout/letters-layout.component.ts
+++ src/app/components/letters-layout/letters-layout.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './letters-layout.component.scss'
 })
 export class LettersLayoutComponent {
+  keyLetters =[
+    ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
+    ['J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'],
+    ['S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  ]
 
 }
