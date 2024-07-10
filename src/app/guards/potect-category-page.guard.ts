6808141067c2d545e2992889225d6170cfb992d8
eawebmaster20 <eaweb20@gmail.com>
2024-07-10 13:07:13 +0000

    core: modified route guard

--- src/app/guards/potect-category-page.guard.ts
+++ src/app/guards/potect-category-page.guard.ts
@@ -1,5 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { DataStateService } from '../services/data-state.service';
 
-export const potectCategoryPageGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+export class potectCategoryPageGuard implements CanActivate {
+  constructor(private router: Router,private dataStateService:DataStateService){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.dataStateService.categories) {
+      return true;
+    }
+    this.router.navigate(['/']);
+    return false;
+  }
+}
