83a695a7ff1a29fd04d49c99ea4eefa1dc7ae92e
Harriet Buadee <117269443+Harr-izle@users.noreply.github.com>
2024-07-11 16:45:28 -0700

    creates you lose logic

--- src/app/services/data-state.service.ts
+++ src/app/services/data-state.service.ts
@@ -31,9 +31,18 @@ export class DataStateService {
   }
 
   detectWinOrLoss() {
+    console.log('health value: ' + this.healthValue)
+    if (this.healthValue <= 0) {
+      console.log('you failed');
+      return;
+    }
    if(this.healthValue > 0 && this.userWrongGuesses.length < 4){
     for(let i = 0; i < this.chosenPhraseArray.length; i++){
-      
+      for(let j = 0; j < this.userGuessedChars.length; j++){
+        if (this.chosenPhraseArray[i] === this.userGuessedChars[j]) {
+          this.chosenPhraseArray[i] = this.userGuessedChars[j]
+        }
+      }
     }
              console.log("You win"); 
    }
