c3bb2bf51bf4e17512b2d6345b4a1676ea118bce
Harriet Buadee <117269443+Harr-izle@users.noreply.github.com>
2024-07-12 14:55:59 -0700

    changes keyboard layout to the normal qwerty keyboard

--- src/app/components/letters-layout/letters-layout.component.ts
+++ src/app/components/letters-layout/letters-layout.component.ts
@@ -14,9 +14,9 @@ export class LettersLayoutComponent {
   @Output() getClickedKeyEvent = new EventEmitter<string>();
 
   keyLetters = [
-    ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
-    ['J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'],
-    ['S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O','P'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+    ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
   ];
 
   keyClicked(event: any) {
