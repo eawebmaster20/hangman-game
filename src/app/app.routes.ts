dd8e4fb904943ea430eb56a14ee18d371cede407
IT Compliance <compliance@amalitech.org>
2024-07-12 10:22:55 -0700

    Routing for Continue button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { StartGameComponent } from './components/start-game/start-game.component';
 import { ProtectGameGuard } from './guards/protect-game.guard';
 import { protectCategoryPageGuard } from './guards/potect-category-page.guard';
+import { SwitchComponent } from './components/main-game/main-game.component';
 
 export const routes: Routes = [
   { path: '', component: StartGameComponent },
+  { path: 'temp',component:SwitchComponent},
   {
     path: 'main-game',
     loadComponent: () =>
--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
+import { DataStateService } from '../../services/data-state.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-modal',
@@ -10,12 +12,26 @@ import { ModalService } from '../../services/modal.service';
 })
 export class ModalComponent implements OnInit {
   private audio!: HTMLAudioElement;
-  
+  constructor(private router: Router){}
 
   ngOnInit(): void {
     this.audio = new Audio();
   }
+  playNewGame(){
+    console.log('called playgame');
+    this.router.navigate(['temp']);
+    this.dataStateService.userGuessedChars = []
+    this.modalService.toggleModal()
+  }
+  resumeGame(){
+    this.modalService.toggleModal()
+  }
 
+  continueGame(){
+    this.playClickOptionSound()
+    console.log(this.dataStateService.healthValue);
+    this.dataStateService.healthValue===0 ? this.playNewGame(): this.resumeGame()
+  }
   playHoverSound(): void {
     this.audio.src = '../assets/sounds/hover sound.wav'; // Adjust the path to your sound file
     this.audio.load();
@@ -29,4 +45,5 @@ export class ModalComponent implements OnInit {
   }
 
   modalService = inject(ModalService);
+  dataStateService = inject(DataStateService);
 }
